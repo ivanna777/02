2266748f581036cf31dbbee9f8e359d272343a8e
Ivanna Kirij <ivannakirij@gmail.com>
2021-03-25 02:34:50 +0200

    done

--- src/apiService.js
+++ src/apiService.js
@@ -1,16 +1,34 @@
-function fetchPicture(searchPicture) { 
-    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchPicture}&page=1&per_page=12&key=20828965-4298cb7d6f87f653ca708a263`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.hits)
-        return data.hits
-    });
-}
+export default class ApiService {
+    constructor() {
+      this.searchPicture = '';
+      this.page = 1;
+    }
+  
+    fetchPictures() {
+      const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchPicture}&page=${this.page}&per_page=12&key=20828965-4298cb7d6f87f653ca708a263`;
 
-export default {fetchPicture}
+      return fetch(url)
+        .then(response => response.json())
+        .then(pictures => {
+          this.incrementPage();
+          return pictures.hits;
+        });
+    }
+  
+    incrementPage() {
+      this.page += 1;
+    }
+  
+    resetPage() {
+      this.page = 1;
+    }
+  
+    get query() {
+      return this.searchPicture;
+    }
+  
+    set query(newQuery) {
+      this.searchPicture = newQuery;
+    }
+  }
 
-//     const KEY_API = '20828965-4298cb7d6f87f653ca708a263';
-// const BASE_URL = 'https://pixabay.com/api/';
-
-    // const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchPicture}&page=1&per_page=12&key=20828965-4298cb7d6f87f653ca708a263`
-    // const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchPicture}&page=1&per_page=12&key=${KEY_API}`;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,47 @@
-import API from "./apiService";
+import ApiService from "./apiService";
 import pictureTpl from "../src/picture.hbs";
-console.log(pictureTpl)
+import "../src/styles.css"
 
 const inputRef = document.querySelector('.js-input');
 const galleryRef = document.querySelector('.gallery');
-// console.dir(inputRef)
+const loadMoreBtn = document.querySelector('.btn');
 
+const apiService = new ApiService();
 
-inputRef.addEventListener('input', findPicture);
+inputRef.addEventListener('input', onSearch);
+loadMoreBtn.addEventListener('click', fetchPictures);
+loadMoreBtn.addEventListener('click', scroll);
 
+function onSearch(e) {
+  e.preventDefault();
 
-function findPicture() {
-    const searchPicture = inputRef.value;
-    console.log(searchPicture)
+  apiService.query = inputRef.value;
 
-    API.fetchPicture(searchPicture)
-    .then(renderPictures)
-    .catch(error => console.log(error))
+  apiService.resetPage();
+  clearPicturesContainer();
+  fetchPictures();
 }
 
-function renderPictures(pictures) {
-    console.log(pictures)
-    const markup = pictureTpl(pictures);
-    console.log(markup)
-    galleryRef.insertAdjacentElement('afterbegin', markup)
+function fetchPictures() {
+    apiService.fetchPictures().then(pictures => {
+    pictureMarkup(pictures);
+  });
 }
+
+function pictureMarkup(pictures) {
+    galleryRef.insertAdjacentHTML('beforeend', pictureTpl(pictures));
+}
+
+function clearPicturesContainer() {
+    galleryRef.innerHTML = '';
+}
+
+function scroll() {
+    const totalHeight = (galleryRef.clientHeight) + 80;
+    console.log(galleryRef.clientHeight)
+
+    setTimeout(() => {
+        window.scrollTo(0, totalHeight);
+    }, 2000)
+}
+
